292ef5a6c860b58f8916d0c1ea098ffab1b3f247
Vel <velocfc@gmail.com>
2022-04-25 22:39:49 +0800

    优化路由

--- js/main_control_functions.js
+++ js/main_control_functions.js
@@ -5,3 +5,9 @@ function open_loading_preview(){  // 开启加载动画。
 function close_loading_preview(){ // 关掉加载动画。
   document.getElementById("loading-preview-animation").setAttribute("class", "spinner-no-more");
 }
+
+function clear_nav_hightlight(){  // 取消导航栏高亮
+  for (let item of document.getElementsByClassName("layui-this")){
+      item.classList.remove("layui-this");
+    }
+}
