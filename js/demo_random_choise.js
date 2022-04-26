fccfb6a1bb4cf292f2130aa12d89a000813983ce
Vel <velocfc@gmail.com>
2022-04-26 21:52:06 +0800

    Update demo_random_choise.js

--- js/demo_random_choise.js
+++ js/demo_random_choise.js
@@ -3,14 +3,14 @@ class RandomChoiseDemo {
 
   create_demo_random_choise(element_id){
     open_loading_preview();
-    $("#"+element_id).load("static_content/genshin-purple-choise-animation.html");
-
-    setTimeout(function (){
-      if(window.location.hash == "#/demo3/"){
-        $("#"+element_id).load("static_content/random-choise-animation.html");
-      }
-    }, 3400);
+    $("#"+element_id).load("static_content/genshin-purple-choise-animation.html", function(){
+      setTimeout(function (){
+        if(window.location.hash == "#/demo3/"){
+          $("#"+element_id).load("static_content/random-choise-animation.html");
+        }
+      }, 3400);
     
-    close_loading_preview();
+      close_loading_preview();
+    });
   }
 }
