3c70f5ba3c539e2322ed42c95c98bd9482748cf5
Vel <velocfc@gmail.com>
2022-06-17 09:35:42 +0800

    文本分析改动

--- js/data_analysis.js
+++ js/data_analysis.js
@@ -165,69 +165,111 @@ class DataAnalysis {
     open_loading_preview();
     // console.log(element_id)
     // console.log(document.getElementById(element_id))
-    document.getElementById(element_id).innerHTML = "数据分析模块初始化……";
+    document.getElementById(element_id).innerHTML = "文本数据分析模块初始化……";
+    var current_index_data_text_analysis = 0;
     
     $("#"+element_id).load("static_content/data_text_analysis_page.html", function(){
-      // analysis_element_list 必须与 data_analysis_page.html 中的 tab 顺序一致
-      var analysis_element_list = [
-        "locations",
-        "educations_school",
-        "educations_major",
-        "employments_job",
-        "employments_company",
-        "business",
-        "gender",
-        "user_type",
-        "voteup_count",
-        "favorited_count",
-        "thanked_count",
-        "answer_count",
-        "articles_count",
-        "follower_count",
-        "following_count",
-      ];
-      let idx = 0;
-      $.getJSON("data/count_analytics/"+analysis_element_list[idx]+"_count.json").then(data => {
-        getDataAnalysisPiePlot("pie-" + analysis_element_list[idx], data);
-        getDataAnalysisBarPlot("bar-" + analysis_element_list[idx], data);
-      })
-      layui.use('element', function(){
-          var element = layui.element;
-        element.on('tab(data_analysis)', function(data){
-          // console.log(this); //当前Tab标题所在的原始DOM元素
-          // console.log(data.index); //得到当前Tab的所在下标
-          // console.log(data.elem); //得到当前的Tab大容器
-          let idx = data.index;
-          if (! analysis_element_list[idx].includes("_count")){
-            $.getJSON("data/count_analytics/"+analysis_element_list[idx]+"_count.json").then(data => {
-              getDataAnalysisBarPlot("bar-" + analysis_element_list[idx], data);
-              getDataAnalysisPiePlot("pie-" + analysis_element_list[idx], data);
-            })
-          }
-          else{
-            function show_all_counts(idx, maxidx){
-              if(idx < maxidx){
-                $.getJSON("data/count_analytics/"+analysis_element_list[idx]+"_count.json").then(data => {
-                  getDataAnalysisPiePlot("pie-" + analysis_element_list[idx], data, true, false);
-                }).then( nothing => {
-                  show_all_counts(idx + 1, maxidx);
-                })
-              }
-              else return;
-            }
-            show_all_counts(idx, analysis_element_list.length);
-            // while(idx < analysis_element_list.length){
-            //   console.log("idx:", idx);
-            //   console.log("analysis_element_list[idx]:", analysis_element_list[idx]);
-            //   $.getJSON("data/count_analytics/"+analysis_element_list[idx]+"_count.json").then(data => {
-            //     getDataAnalysisPiePlot("pie-" + analysis_element_list[idx], data, true);
-            //   });
-            //   idx += 1;
-            // }
-            
+      // 注意这里只取了 1w 条
+      $.post(
+        'http://407s2.dns.navy:2334/match_fields_count', 
+        {},
+        function(data){
+          // 数据可视化
+          console.log("[开始文本数据分析]");
+          document.getElementById("data-visualization").setAttribute("style", "visibility: visible;");
+          let attr_key_dict = {
+            0: "locations",
+            1: "educations_school",
+            2: "educations_major",
+            3: "employments_job",
+            4: "employments_company",
+            5: "business",
+            6: "extract_words",
           }
           
-        });
+          function plot_everything(idx){
+            console.log('attr_key_dict[idx]:', attr_key_dict[idx]);
+            if (! attr_key_dict[idx].includes("_count")){
+              let attr = attr_key_dict[idx];
+              if (attr == "locations" || attr == "business"){
+                for(let key in data[attr]){
+                  if(data[attr][key] == "") delete data[attr][key];
+                }
+                // getDataAnalysisPiePlot("pie-" + attr, data[attr]);
+                getDataAnalysisBarPlot("bar-" + attr, data[attr]);
+                console.log("[getDataAnalysisWordCloudPlot]", attr);
+                getDataAnalysisWordCloudPlot("wordcloud-" + attr, data[attr]);
+              }
+              else if(attr.includes("educations") || attr.includes("employments")){
+                let lv1 = attr.split("_")[0], lv2 = attr.split("_")[1];
+                // console.log("data:", data)
+                // console.log("lv1:", lv1)
+                // console.log("lv2:", lv2)
+                // getDataAnalysisPiePlot("pie-" + attr, data[lv1][lv2]);
+                getDataAnalysisBarPlot("bar-" + attr, data[lv1][lv2]);
+                getDataAnalysisWordCloudPlot("wordcloud-" + attr, data[lv1][lv2]);
+              }
+              else if(attr == "gender"){
+                if (1 in data[attr]){
+                  data[attr]['男'] = data[attr][1];
+                  data[attr]['女'] = data[attr][-1];
+                  data[attr]['未知'] = data[attr][0];
+                  delete data[attr][1];
+                  delete data[attr][-1];
+                  delete data[attr][0];
+                }
+                // getDataAnalysisPiePlot("pie-" + attr, data[attr]);
+                getDataAnalysisWordCloudPlot("wordcloud-" + attr, data[attr]);
+                getDataAnalysisBarPlot("bar-" + attr, data[attr]);
+              }
+              else if(attr == "user_type"){
+                // getDataAnalysisPiePlot("pie-" + attr, data[attr]);
+                getDataAnalysisWordCloudPlot("wordcloud-" + attr, data[attr]);
+                getDataAnalysisBarPlot("bar-" + attr, data[attr]);
+              }
+              else if(attr == "extract_words"){
+                open_loading_preview();
+                $.getJSON("data/count_analytics/"+attr+"_count.json").then(data =>{
+                  console.log("data:", data);
+                  getDataAnalysisWordCloudPlot("wordcloud-extract_words", data);
+                  getDataAnalysisBarPlot("bar-" + attr, data);
+                  getDataAnalysisPiePlot("pie-" + attr, data);
+                })
+                .then(data => {
+                  close_loading_preview();
+                })
+                
+              }
+            }
+            else{
+              function show_all_counts(idx, maxidx){
+                console.log("数值活跃度……", idx, maxidx)
+                let attr = attr_key_dict[idx];
+                if(idx < maxidx && attr.includes("_count")){
+                  getDataAnalysisPiePlot("pie-" + attr, data[attr], true, false);
+                  show_all_counts(idx + 1, maxidx);
+                }
+                else return;
+              }
+              show_all_counts(idx, Object.keys(attr_key_dict).length);
+            }
+          }
+
+          
+          plot_everything(current_index_data_text_analysis);
+
+          layui.use('element', function(){
+            var element = layui.element;
+            element.on('tab(data_analysis)', function(plot_ele){
+              let idx = plot_ele.index;
+              plot_everything(idx);
+              current_index_data_text_analysis = idx;
+            });
+          });
+        }
+      ).then( data =>{
+        document.getElementById("data-visualization").setAttribute("style", "visibility: visible;");
+        close_loading_preview();
       });
       close_loading_preview();
     });
@@ -444,6 +486,7 @@ function getDataAnomalyAnalysisBarPlot(plot_element_id, data){
       myChart.setOption(option);
 }
 
+<<<<<<< Updated upstream
 //----------------------------------------------------------------//
 function getDataAdvanedAnalysisParallelPlot(plot_element_id, data){
   let myChart = echarts.init(document.getElementById(plot_element_id));
@@ -467,4 +510,63 @@ function getDataAdvanedAnalysisParallelPlot(plot_element_id, data){
     }
   };
   myChart.setOption(option);
+=======
+function getDataAnalysisWordCloudPlot(plot_element_id, data){
+  let atttribute_name = plot_element_id.split("-")[1];
+  // let atttribute_name = plot_element_id.replace("_count", "");
+  console.log("getDataAnalysisWordCloudPlot:",translate_columns[atttribute_name], "data:", data)
+  let data_list = [];
+  for(let key in data){
+    data_list.push({"name": key, "value": data[key]});
+  }
+  data_list.sort(function(a, b){
+    return b["value"] - a["value"];
+  })
+  
+  data_list = data_list.slice(0,200);
+  // console.log("data_list:", data_list);
+  
+  let myColorList = [
+    d3.scaleSequential().domain([0, data_list.length]).interpolator(d3.interpolateRainbow),
+    d3.scaleSequential().domain([0, data_list.length]).interpolator(d3.interpolateSinebow),
+    d3.scaleSequential().domain([0, data_list.length]).interpolator(d3.interpolateTurbo),
+  ];
+  let myColor = myColorList[Math.floor(Math.random() * myColorList.length)];
+  let myChart = echarts.init(document.getElementById(plot_element_id));
+      let option = {
+        tooltip: {},
+        series: [
+          {
+            type: 'wordCloud',
+            gridSize: 2,
+            sizeRange: [12, 50],
+            rotationRange: [-90, 90],
+            shape: 'pentagon',
+            width: '100%',
+            height: '100%',
+            sizeRange: [20, 85],
+            // drawOutOfBound: true,
+            textStyle: {
+                color: function () {
+                    return 'rgb(' + [
+                        Math.round(Math.random() * 160),
+                        Math.round(Math.random() * 160),
+                        Math.round(Math.random() * 160)
+                    ].join(',') + ')';
+                }
+            },
+            emphasis: {
+                textStyle: {
+                    shadowBlur: 10,
+                    shadowColor: '#333'
+                }
+            },
+            data: data_list,
+            name: translate_columns[atttribute_name],
+          }
+        ]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+>>>>>>> Stashed changes
 }
